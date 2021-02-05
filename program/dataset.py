a524f29edbf910bf1050cee39268ff13eecfa3f6
mayersjd <mayersjd@mail.uc.edu>
2021-02-05 18:10:52 -0500

    Reorganized file structure and function definitions to be more manageable.

--- program/dataset.py
+++ program/dataset.py
@@ -1,6 +1,10 @@
-import os
 from pathlib import Path
 import glob
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import os
+import datetime as dt
 
 
 def formatFiles():
@@ -8,8 +12,8 @@ def formatFiles():
 
     stockDataPath = os.path.join(currentDirectory, r'historicaldata\stocks')    # path to historical data for stocks
     ETFDataPath = os.path.join(currentDirectory, r'historicaldata\ETFs')    # path to historical data for ETFs
-    stockFiles = Path(stockDataPath).glob('*.txt')  # make a list of all the .txt files
-    ETFFiles = Path(ETFDataPath).glob('*.txt')  # make a list of all the .txt files
+    stockFiles = Path(stockDataPath).glob('*.txt')  # make a generator of all the .txt files
+    ETFFiles = Path(ETFDataPath).glob('*.txt')  # make a generator of all the .txt files
 
     # Rename all the .txt files to .csv
     for file in stockFiles:
@@ -18,6 +22,85 @@ def formatFiles():
         file.replace(file.with_suffix('.csv'))
 
 
-def formatData():
+def formatData(inputSize, forecast):
+    print('Reading in and formatting data files for training and testing...')
+    # Read in the data set for training
+    currentDirectory = os.getcwd()  # Get the current working directory
+    stockDataPath = os.path.join(currentDirectory, r'historicaldata\stocks')
+    stockFiles = Path(stockDataPath).glob('*.csv')  # make a generator of all the .csv files
 
-    return
+    count = 0
+    labeledData = []
+    for file in stockFiles:
+        if count > 0:
+            break
+        else:
+            if os.stat(file).st_size == 0:
+                continue
+            else:
+                data = pd.read_csv(file, parse_dates=['Date'])     # Read in the data for a stock, and change the date information to datetime objects
+                data.pop('OpenInt')     # Remove the 'OpenInt' column, as it appears to be all zeroes all the time
+
+                # Convert the values in the Date column to integers referencing the number of days passed since 1900-1-1
+                for i in range(len(data)):
+                    data['Date'][i] = float((data['Date'][i] - dt.datetime(1900, 1, 1)).days)   # This raises an error, but I don't think it's a problem
+                data['Volume'] = data['Volume'].astype(float)
+
+                # Splitting the dataframe up into chunks of the appropriate input size
+                split_remainder = len(data) % inputSize  # Check if the current size of the dataframe is divided evenly by the desired input size
+                data.drop(data.tail(split_remainder).index, inplace=True)   # reshape the dataframe by dropping the remainder
+                splitSize = len(data) / inputSize
+                dataSplit = np.array_split(data, splitSize)     # split the data frame into equally sized chunks
+
+                # Call function to create the labels for training/validation
+                labeledData += (createLabels(data, dataSplit, forecast))
+        count += 1
+
+    # Call function to shuffle the data and split into testing and training sets
+    trainingSet, trainingLabels, testingSet, testingLabels = shuffleData(labeledData)
+
+    print('Done!')
+    return trainingSet, trainingLabels, testingSet, testingLabels
+
+
+def createLabels(data, dataSplit, forecast):
+    labels = []
+    for i, split in enumerate(dataSplit):
+        futureIndex = i * len(split) + forecast
+        if futureIndex >= len(data):
+            # dataSplit = dataSplit[:-(len(dataSplit)-i)]
+            break
+        else:
+            currentValueAvg = (split['Open'][i * len(split)] + split['High'][i * len(split)] + split['Low'][i * len(split)] + split['Close'][i * len(split)]) / 4
+            futureValueAvg = (data['Open'][futureIndex] + data['High'][futureIndex] + data['Low'][futureIndex] + data['Close'][futureIndex]) / 4
+            if futureValueAvg > currentValueAvg:
+                labels.append([split.values, 1])    # Label of 1 indicates the stock rises in price over the desired timeframe
+            else:
+                labels.append([split.values, 0])    # Label of 0 indicates the stock falls in price over the desired timeframe
+    return labels
+
+def shuffleData(labeledData):
+    # Shuffle the data, then split it into training and validation sets
+    np.random.shuffle(labeledData)
+    trainingDataLength = round(len(labeledData) * 0.7)
+    trainingSet = []
+    trainingLabels = []
+    testingSet = []
+    testingLabels = []
+    for i in range(len(labeledData)):
+        if i <= trainingDataLength:
+            trainingSet.append(labeledData[i][0])
+            trainingLabels.append(labeledData[i][1])
+        else:
+            testingSet.append(labeledData[i][0])
+            testingLabels.append(labeledData[i][1])
+
+    # We have to convert the list of arrays to a multidimensional numpy array and then convert it into a tensor object
+    trainingSet = np.asarray(trainingSet).astype(np.float32)
+    trainingSet = tf.convert_to_tensor(trainingSet)   # Converting the training set to a tensor
+    trainingLabels = np.asarray(trainingLabels).astype(np.uint8)    # Converting the labels to a numpy array
+    testingSet = np.asarray(testingSet).astype(np.float32)
+    testingSet = tf.convert_to_tensor(testingSet)   # Converting the testing set to a tensor
+    testingLabels = np.asarray(testingLabels).astype(np.uint8)    # Converting the labels to a numpy array
+
+    return trainingSet, trainingLabels, testingSet, testingLabels
--- program/machine.py
+++ program/machine.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
-import numpy as np
-import pandas as pd
-import os
-import datetime as dt
+from program import dataset
 
 
 def go():
@@ -10,7 +7,7 @@ def go():
     # Copied from" https://stackoverflow.com/questions/41117740/tensorflow-crashes-with-cublas-status-alloc-failed
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        # Restrict TensorFlow to only allocate 2GB of memory on the first GPU
         try:
             # Change this limit to make sure you don't exceed available memory on your GPU
             tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
@@ -22,77 +19,18 @@ def go():
 
     # Both of these sort of assume that data is available for every business day, which is not always a valid assumption
     # This influences the size of the input to the NN
-    desiredInputSize = 20  # desired number of training inputs, 1 for day, 5 for week, 20 for month, 240 for year.
+    desiredInputSize = 120  # desired number of training inputs, 1 for day, 5 for week, 20 for month, 240 for year.
     # This changes the labels for training/validation
     desiredPredictionRange = 20  # desired timeframe for inferencing, 1 for day, 5 for week, 20 for month, 240 for year.
 
-    # Read in the data set for training
-    currentDirectory = os.getcwd()  # Get the current working directory
-    stockDataPath = os.path.join(currentDirectory, r'historicaldata\stocks\a.us.csv')
-    data = pd.read_csv(stockDataPath, parse_dates=['Date']) # Read in the data for a stock, and change the date information to datetime objects
-    data.pop('OpenInt')     # Remove the 'OpenInt' column, as it appears to be all zeroes all the time
-
-    # Convert the values in the Date column to integers referencing the number of days passed since 1900-1-1
-    for i in range(len(data)):
-        data['Date'][i] = float((data['Date'][i] - dt.datetime(1900, 1, 1)).days)   # This raises an error, but I don't think it's a problem
-    data['Volume'] = data['Volume'].astype(float)
-
-    # Splitting the dataframe up into chunks of the appropriate input size
-    split_remainder = len(data) % desiredInputSize  # Check if the current size of the dataframe is divided evenly by the desired input size
-    data.drop(data.tail(split_remainder).index, inplace=True)   # reshape the dataframe by dropping the remainder
-    splitSize = len(data) / desiredInputSize
-    dataSplit = np.array_split(data, splitSize)     # split the data frame into equally sized chunks
-
-    # Call function to create the labels for training/validation
-    labeledData = createLabels(data, dataSplit, desiredPredictionRange)
-    #print(labeledData[0])
-
-    # Shuffle the data, then split it into training and validation sets
-    np.random.shuffle(labeledData)
-    trainingDataLength = round(len(labeledData) * 0.7)
-    trainingSet = []
-    trainingLabels = []
-    testingSet = []
-    testingLabels = []
-    for i in range(len(labeledData)):
-        if i <= trainingDataLength:
-            trainingSet.append(labeledData[i][0])
-            trainingLabels.append(labeledData[i][1])
-        else:
-            testingSet.append(labeledData[i][0])
-            testingLabels.append(labeledData[i][1])
-    # We have to convert the list of arrays to a multidimensional numpy array and then convert it into a tensor object
-    trainingSet = np.asarray(trainingSet).astype(np.float32)
-    trainingSet = tf.convert_to_tensor(trainingSet)   # Converting the training set to a tensor
-    trainingLabels = np.asarray(trainingLabels).astype(np.uint8)    # Converting the labels to a numpy array
-    testingSet = np.asarray(testingSet).astype(np.float32)
-    testingSet = tf.convert_to_tensor(testingSet)   # Converting the testing set to a tensor
-    testingLabels = np.asarray(testingLabels).astype(np.uint8)    # Converting the labels to a numpy array
+    (trainingSet, trainingLabels, testingSet, testingLabels) = dataset.formatData(desiredInputSize, desiredPredictionRange)
 
     multiLayerPerceptron(trainingSet, trainingLabels, testingSet, testingLabels, desiredInputSize)
 
     return print("Stonks go up!")
 
-
-def createLabels(data, dataSplit, forecast):
-    labels = []
-    for i, split in enumerate(dataSplit):
-        futureIndex = i * len(split) + forecast
-        if futureIndex >= len(data):
-            # dataSplit = dataSplit[:-(len(dataSplit)-i)]
-            break
-        else:
-            currentValueAvg = (split['Open'][i * len(split)] + split['High'][i * len(split)] + split['Low'][i * len(split)] + split['Close'][i * len(split)]) / 4
-            futureValueAvg = (data['Open'][futureIndex] + data['High'][futureIndex] + data['Low'][futureIndex] + data['Close'][futureIndex]) / 4
-            if futureValueAvg > currentValueAvg:
-                labels.append([split.values, 1])    # Label of 1 indicates the stock rises in price over the desired timeframe
-            else:
-                labels.append([split.values, 0])    # Label of 0 indicates the stock falls in price over the desired timeframe
-    return labels
-
-
 def multiLayerPerceptron(trainData, trainLabels, testData, testLabels, inputs):
-
+    print('Building and training multi layer perceptron')
     # Neural network model: Sequential
     mlp = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(inputs, 6)),
